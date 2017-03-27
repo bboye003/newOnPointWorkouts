6977c2514a64893a915791d090934adb8891d128
bboye003 <bboye003@plattsburgh.edu>
2017-03-27 06:43:04 -0400

    Initial Commit

---
+++ newOnPointWorkouts/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  newOnPointWorkouts
+//
+//  Created by Bret Boyer on 3/27/17.
+//  Copyright © 2017 OnPoint Workouts. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
